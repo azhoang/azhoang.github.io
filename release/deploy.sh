ba3de9df353693ae253fc9496490d292bec01988
hhoangg <hoangxuan2402@gmail.com>
2022-09-25 22:07:29 +0700

    update version deploy

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 version=$(uuidgen)
-file_contents=$(<index.html)
+file_contents=$(<dist/index.html)
 echo "${file_contents//__VERSION__/$version}" >index.html
 echo "Version: $version"
