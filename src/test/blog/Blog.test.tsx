0a2712b9889c9930cbd99adfe2253676df5876b5
hoang.xuan.sts <hoang.nguyenx@saigontechnology.com>
2022-09-26 21:34:28 +0700

    update blog test 

--- src/test/blog/Blog.test.tsx
+++ src/test/blog/Blog.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../../pages/blog/Blog';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../../pages/blog/Blog";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Blog/i);
   expect(linkElement).toBeInTheDocument();
